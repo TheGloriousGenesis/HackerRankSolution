2cbea5465178734976bbea5bff0596c3c3c7705b
TheGloriousGenesis <am16g14@soton.ac.uk>
2019-12-19 00:18:59 +0000

    Implementing algorithm...

--- src/Solutions/Medium/TheStoryOfATree.java
+++ src/Solutions/Medium/TheStoryOfATree.java
@@ -1,77 +1,107 @@
 package Solutions.Medium;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static java.util.Arrays.asList;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class TheStoryOfATree {
-//    // different number of games 1<q<5
-//    int q = 0;
-//    // number of nodes 1<n<10^5
-//    int n = 0;
-//    // number of guesses 1< g <10^5
-//    int g = 0;
-//    // minimum score needed to win 1< k <10^5
-//    int k = 0;
-//
-
-    // different number of games
-    int q = 2;
-    // number of nodes
-    int n = 4;
-    // number of guesses
-    int g = 2;
-    // minimum score needed to win
-    int k = 2;
-
-    int parent = 0;
-
-    int child = 0;
-
-    int[][] guess = new int[g][1];
-
-
     static String storyOfATree(int n, int[][] edges, int k, int[][] guesses) {
 
-        float winPercentage = k/guesses.length <= 1 ? k/guesses.length : 0;
-        int winCounter = 0;
+        String winPercentage = "";
 
-        Map<Integer, List<Integer>> parentChildNodes = parentChildMap(edges);
+        Map<Integer, List<Integer>> parentChildNodes = nodeMap(edges);
 
+        List<Integer> canWinIfRoot = findGuessMatch(k, guesses, parentChildNodes);
 
-        for(int i=0; i<guesses.length; i++){
-            int childNode = guesses[i][1];
-            int parentNodeGuess = guesses[i][0];
-            int actualParentNode = findParent(childNode, parentChildNodes);
-
-            if (actualParentNode == parentNodeGuess) {
-                winCounter++;
-            }
+        if (canWinIfRoot.isEmpty()) {
+            winPercentage = "0" + "/" + n;
+        } else {
+            winPercentage = canWinIfRoot.size() + "/" + n;
         }
 
-        if (winCounter >= k) {
 
-        }
-
-        return "lo";
+        return winPercentage;
     }
 
-    private static int findParent(int childNode, Map<Integer, List<Integer>> parentNodes) {
-    return 0;
+    private static List<Integer> findGuessMatch(int k, int[][] guess, Map<Integer, List<Integer>> parentNodes) {
+        List<Integer> winningRoots = new ArrayList<>();
+        for(int i=0; i<guess.length; i++) {
+            int parentNode = guess[i][0];
+            int childNode = guess[i][1];
+
+        }
+        if (winningRoots.size() >= k) {
+            return winningRoots;
+        }
+        Map<Integer, List<Integer>> cloneNodeMap = new HashMap<>();
+        cloneNodeMap.putAll(parentNodes);
+
+        parentNodes.entrySet().forEach(i ->{
+            int root = i.getKey();
+            List<Integer> childrens = i.getValue();
+            List<Integer> parents = i.getValue();
+            childrens.forEach(child -> {
+                List<Integer> children = cloneNodeMap.get(child);
+                if ((children != null && !children.isEmpty())) {
+                    children.remove(root);
+                }
+                if (children.isEmpty()) {
+                    parents.remove(child);
+                }
+            });
+        });
+        return winningRoots;
     }
 
     public static void main(String[] args) {
         int[][] testValues = {{1,2}, {1,3}, {3,4}};
-        TheStoryOfATree.parentChildMap(testValues)
+        TheStoryOfATree.drawTree(1, testValues)
         .entrySet().forEach(entry -> {
             System.out.println(entry.getKey() + " " + entry.getValue());
         });
     }
 
-    public static Map<Integer, List<Integer>> parentChildMap(final int[][] edges) {
+    public static Map<Integer, List<Integer>> drawTree(final int root, int[][] edges){
+        // returns map showing the parent to child mapping for a given root
+        Map<Integer, List<Integer>> nodeMap = new HashMap<>();
+
+        int[] distinctNodeValues = Arrays.stream(edges).flatMapToInt(Arrays::stream).distinct().toArray();
+
+        List<Integer> orphanChildList = Arrays.stream(distinctNodeValues).boxed().collect(Collectors.toList());
+        for (int i: distinctNodeValues) {
+            nodeMap.put(i, new ArrayList<>());
+        }
+
+        List<Integer> children = nodeMap.get(root);
+        for(int i=0; i<edges.length; i++) {
+            int x1 = edges[i][0];
+            int x2 = edges[i][1];
+            if (x1 == root && orphanChildList.contains(x2)) {
+                children.add(x2);
+                orphanChildList.remove(x2);
+            }
+            if (x2 == root && orphanChildList.contains(x1)) {
+                children.add(x1);
+                orphanChildList.remove(x1);
+            }
+        }
+        nodeMap.put(root, children);
+
+        for (int j: children) {
+            List<Integer> children_2 = nodeMap.get(j);
+            for(int i=0; i<edges.length; i++) {
+                int x1 = edges[i][0];
+                int x2 = edges[i][1];
+                if (x1 == root && orphanChildList.contains(x2)) children_2.add(x2);
+                if (x2 == root && orphanChildList.contains(x1)) children_2.add(x1);
+            }
+            nodeMap.put(j, children_2);
+        }
+
+        return nodeMap;
+    }
+
+    public static Map<Integer, List<Integer>> nodeMap(final int[][] edges) {
         Map<Integer, List<Integer>> parentChildMap = new HashMap<>();
 
         for (int i =0;i< edges.length; i++) {
@@ -85,8 +115,22 @@ public class TheStoryOfATree {
                 parentChildMap.put(edges[i][0], childNode);
             }
         }
+
+        for (int i =0;i< edges.length; i++) {
+            if (parentChildMap.get(edges[i][1]) == null) {
+                List<Integer> childNodes = new ArrayList<Integer>();
+                childNodes.add(edges[i][0]);
+                parentChildMap.put(edges[i][1], childNodes);
+            } else {
+                List<Integer> childNode = parentChildMap.get(edges[i][1]);
+                childNode.add(edges[i][0]);
+                parentChildMap.put(edges[i][1], childNode);
+            }
+        }
         return parentChildMap;
     }
 }
+
+
 
 
